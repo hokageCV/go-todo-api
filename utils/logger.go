dc2d774683b58f225d242b7a13421f5c8d4e5ad1
hokageCV <chaitanyavaru@gmail.com>
2023-07-03 17:05:54 +0530

    add logger

---
+++ utils/logger.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.Logger
+
+func InitializeLogger() {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	Logger, _ = config.Build()
+}
+
+func GetLogger() *zap.Logger {
+	return Logger
+}
