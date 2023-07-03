e39f1d2adb945b74edb6434bda5ba6563c8087e5
hokageCV <chaitanyavaru@gmail.com>
2023-07-03 17:51:41 +0530

    send error also

--- storage/db_operations.go
+++ storage/db_operations.go
@@ -6,25 +6,28 @@ import (
 	"go.uber.org/zap"
 )
 
-func InsertIntoDB(title string) {
+func InsertIntoDB(title string) error {
 	db := GetDB()
 	logger := utils.GetLogger()
 
 	stmt, err := db.Prepare("INSERT INTO todos (title, is_done) VALUES ($1, $2)")
 	if err != nil {
 		logger.Error("🛑 can't prepare statement ", zap.Error(err))
+		return err
 	}
 	defer stmt.Close()
 
 	_, err = stmt.Exec(title, false)
 	if err != nil {
 		logger.Error("🛑 can't execute statement ", zap.Error(err))
-	} else {
-		logger.Info("✅ successfully inserted into table")
+		return err
 	}
+
+	logger.Info("✅ successfully inserted into table")
+	return nil
 }
 
-func GetAllTodoFromDB() []types.Todo {
+func GetAllTodoFromDB() ([]types.Todo, error) {
 
 	db := GetDB()
 	logger := utils.GetLogger()
@@ -32,6 +35,7 @@ func GetAllTodoFromDB() []types.Todo {
 	rows, err := db.Query("SELECT * FROM todos")
 	if err != nil {
 		logger.Error("🛑 can't query table ", zap.Error(err))
+		return nil, err
 	}
 	defer rows.Close()
 
@@ -42,46 +46,53 @@ func GetAllTodoFromDB() []types.Todo {
 		err = rows.Scan(&task.ID, &task.Title, &task.IsDone)
 		if err != nil {
 			logger.Error("🛑 can't scan row ", zap.Error(err))
+			return nil, err
 		}
 		todoList = append(todoList, task)
 	}
-	logger.Info("✅ successfully fetched all rows from table")
 
-	return todoList
+	logger.Info("✅ successfully fetched all rows from table")
+	return todoList, nil
 }
 
-func UpdateInDB(taskID string) {
+func UpdateInDB(taskID string) error {
 	db := GetDB()
 	logger := utils.GetLogger()
 
 	stmt, err := db.Prepare("UPDATE todos SET is_done = true WHERE id = $1")
 	if err != nil {
 		logger.Error("🛑 can't prepare statement ", zap.Error(err))
+		return err
 	}
 	defer stmt.Close()
 
 	_, err = stmt.Exec(taskID)
 	if err != nil {
 		logger.Error("🛑 can't execute statement ", zap.Error(err))
-	} else {
-		logger.Info("✅ successfully updated row in table")
+		return err
 	}
+
+	logger.Info("✅ successfully updated row in table")
+	return nil
 }
 
-func DeleteFromDB(taskID string) {
+func DeleteFromDB(taskID string) error {
 	db := GetDB()
 	logger := utils.GetLogger()
 
 	stmt, err := db.Prepare("DELETE FROM todos WHERE id = $1")
 	if err != nil {
 		logger.Error("🛑 can't prepare statement ", zap.Error(err))
+		return err
 	}
 	defer stmt.Close()
 
 	_, err = stmt.Exec(taskID)
 	if err != nil {
 		logger.Error("🛑 can't execute statement ", zap.Error(err))
-	} else {
-		logger.Info("✅ successfully deleted row in table")
+		return err
 	}
+
+	logger.Info("✅ successfully deleted row in table")
+	return nil
 }
